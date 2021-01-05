33df0cb0fcafc434e7cb8fec3a10a01c8685a8bf
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-01-05 03:19:26 -0500

    Canelita Image

---
+++ acnh/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MainCanelita from "./pages/MainCanelita";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <MainCanelita></MainCanelita>
+    </div>
+  );
+}
+
+export default App;
